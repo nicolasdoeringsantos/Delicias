598b09e231f931b0596502b560d2dbb9a4235c20
Nicolas Doering Santos <nicolasdoeringsantos@gmail.com>
2026-08-12 11:44:47 -0300

    ajuste preço

--- js/main.js
+++ js/main.js
@@ -10,14 +10,14 @@ import paoMilho from '../images/paomilho.png';
 const WHATS_NUMBER = '5554984163345';
 
 const products = [
-  { name: 'Pão rocambole de beterraba', price: 'R$ 18,00', image: paoBeterraba },
-  { name: 'Biscoitos amanteigados', price: 'R$ 22,00', image: bolachaManteiga },
-  { name: 'Pão integral', price: 'R$ 16,00', image: paoIntegral },
+  { name: 'Pão rocambole de beterraba', price: 'R$ 10,00', image: paoBeterraba },
+  { name: 'Biscoitos amanteigados', price: 'R$ 15,00', image: bolachaManteiga },
+  { name: 'Pão integral', price: 'R$ 10,00', image: paoIntegral },
   { name: 'Massa caseira', price: 'R$ 8,00', image: massaCaseira },
-  { name: 'Bolachas glaceadas', price: 'R$ 12,00', image: bolachaGlaceada },
-  { name: 'Rosquinha de manteiga com chocolate', price: 'R$ 20,00', image: rosquinhaChocolate },
+  { name: 'Bolachas glaceadas', price: 'R$ 15,00', image: bolachaGlaceada },
+  { name: 'Rosquinha de manteiga com chocolate', price: 'R$ 15,00', image: rosquinhaChocolate },
   { name: 'Bolacha de manteiga com goiabada', price: 'R$ 15,00', image: bolachaGoiabada },
-  { name: 'Pão de milho', price: 'R$ 7,00', image: paoMilho }
+  { name: 'Pão de milho', price: 'R$ 10,00', image: paoMilho }
 ];
 
 function buildMenu() {
